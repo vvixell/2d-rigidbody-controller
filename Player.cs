51e9d5729b0518985f444428d2ed7c59116ed873
vvixell <62993193+vvixell@users.noreply.github.com>
2020-11-08 21:59:21 +0000

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    [Header("Movement")]
+    public float moveSpeed;
+    public float jumpHeight;
+
+    [Header("Ground Checking")]
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize;
+
+    public LayerMask mask;
+
+    bool isGrounded;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        //movement
+        Move();
+    }
+    
+    void Move()
+    {
+        float xMove = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        isGrounded = Physics2D.BoxCast(groundCheckPos.position, groundCheckSize, 0, Vector2.zero, 0, mask);
+
+        Vector2 move = new Vector2(xMove, rb.velocity.y);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+            move = new Vector2(rb.velocity.x, jumpHeight);
+
+        rb.velocity = move;
+    }
+}
